2c90a9580ed1e9e4129d5aaee9f09060dfa88d0a
Oskar Breitkopf <oskarbreitkopf@gmail.com>
2024-09-11 09:03:34 -0300

    Initial Commit

---
+++ Firebase_Tutorial_Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Firebase_Tutorial_Swift
+//
+//  Created by Oskar Breitkopf on 11.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Firebase_Tutorial_Swift/Firebase_Tutorial_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_Tutorial_SwiftApp.swift
+//  Firebase_Tutorial_Swift
+//
+//  Created by Oskar Breitkopf on 11.09.24.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_Tutorial_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
